4d5f25bb8b5cf865e84fc8f0eba08e3ce3beb485
Pedro Guimarães Bastos <pedroguibas123@gmail.com>
2024-12-08 15:28:49 -0300

    Criação de cards. Foi implementada a função para criar os cards de modo que os pokemon não podem ser repetidos.

--- javascript/game.js
+++ javascript/game.js
@@ -1,12 +1,33 @@
-function getRandomNumber() {
-    return Math.floor(Math.random() * 1025 + 1);
+function getRandomNumber(blackList) {
+    let rng =  Math.floor(Math.random() * 1025 + 1);
+    while (blackList.includes(rng)) {
+        rng =  Math.floor(Math.random() * 1025 + 1);
+    }
+
+    return rng;
 }
 
 let numberBlackList;
 
 async function fetchPokemon() {
-    let pokemonNumber = getRandomNumber();
-    let response = await fetch('https://pokeapi.co/api/v2/pokemon-species/' + pokemonNumber).catch((err) => console.error(err));
-    let obj = await response.json();
-    console.log(obj.name);
+    numberBlackList = [0, 0];
+    for (let i=0; i<24; i++) {
+        let pokemonNumber = getRandomNumber(numberBlackList);
+        numberBlackList[i] = pokemonNumber;
+        let response = await fetch('https://pokeapi.co/api/v2/pokemon-species/' + pokemonNumber).catch((err) => console.error(err));
+        let obj = await response.json();  
+        addPokemonCard(pokemonNumber, obj.name);
+    }
+}
+
+function addPokemonCard(num, name) {
+    let newCard = document.createElement('div');
+    newCard.classList.add('pokemonCard');
+    let newPokeImg = document.createElement('img');
+    newPokeImg.src = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + num +'.png'
+    let cardTitle = document.createElement('h3');
+    cardTitle.textContent = name;
+    newCard.appendChild(newPokeImg);
+    newCard.appendChild(cardTitle);
+    document.getElementById('gameContainer').appendChild(newCard);
 }
