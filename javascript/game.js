1fedbb1efdd3935b7d25480fd931c302dfa4e2d5
Pedro Guimarães Bastos <pedroguibas123@gmail.com>
2024-12-08 17:35:06 -0300

    Código para gerar tabuleiro inplementei uma função para que quando duas pessoas quiserem jogar elas possam compartilhar um código entre si para que tenham o mesmo tabuleiro.

--- javascript/game.js
+++ javascript/game.js
@@ -7,24 +7,25 @@ function getRandomNumber(blackList) {
     return rng;
 }
 
-let numberBlackList;
-
 async function fetchPokemon() {
-    let gameContainer = document.getElementById('gameContainer');
-    gameContainer.style = 'display: none;'
+    gameCode = '';
+    document.getElementById('gameContainer').innerHTML = '';
+    let mainContainer = document.getElementById('mainContent');
+    mainContainer.style = 'display: none;'
     let spinner = document.getElementById('loadingStatus');
     spinner.style = 'display: block;'
     numberBlackList = [0, 0];
 
     for (let i=0; i<24; i++) {
         let pokemonNumber = getRandomNumber(numberBlackList);
+        gameCode = gameCode + pokemonNumber + ';';
         numberBlackList[i] = pokemonNumber;
         let response = await fetch('https://pokeapi.co/api/v2/pokemon-species/' + pokemonNumber).catch((err) => console.error(err));
         let obj = await response.json();  
         addPokemonCard(pokemonNumber, obj.name);
     }
     spinner.style = 'display: none;'
-    gameContainer.style = 'display: flex;';
+    mainContainer.style = 'display: block;';
 }
 
 function addPokemonCard(num, name) {
@@ -38,3 +39,57 @@ function addPokemonCard(num, name) {
     newCard.appendChild(cardTitle);
     document.getElementById('gameContainer').appendChild(newCard);
 }
+
+function copiaCodigo() {
+    let newInput = document.createElement('input');
+    newInput.setAttribute('value', gameCode);
+    document.body.appendChild(newInput);
+    newInput.select();
+    document.execCommand('copy');
+    newInput.parentNode.removeChild(newInput);
+}
+
+function getGameCodeReady() {
+    let externalGameCode = document.getElementById('inputCodigoExterno').value;
+    let j = 0;
+    coppiedGamePokemonList = ['', ''];
+    for (let i=0; i<externalGameCode.length; i++) {
+        if (externalGameCode.charAt(i) != ';') {
+            coppiedGamePokemonList[j] = coppiedGamePokemonList[j] + externalGameCode.charAt(i)
+        } else {
+            j++;
+            coppiedGamePokemonList[j] = '';
+        }
+    }
+    geraTabuleiroCopiado(coppiedGamePokemonList);
+}
+
+async function geraTabuleiroCopiado(pokemonNumber) {
+    gameCode = '';
+    document.getElementById('gameContainer').innerHTML = '';
+    let mainContainer = document.getElementById('mainContent');
+    mainContainer.style = 'display: none;'
+    let spinner = document.getElementById('loadingStatus');
+    spinner.style = 'display: block;'
+
+    for (let i=0; i<24; i++) {
+        gameCode = gameCode + pokemonNumber[i] + ';';
+        let response = await fetch('https://pokeapi.co/api/v2/pokemon-species/' + pokemonNumber[i]).catch((err) => console.error(err));
+        let obj = await response.json();  
+        addPokemonCard(pokemonNumber[i], obj.name);
+    }
+    spinner.style = 'display: none;'
+    mainContainer.style = 'display: block;';
+}
+
+let numberBlackList;
+let gameCode = '';
+let coppiedGamePokemonList;
+const novoTabuleiroBtn = document.querySelector('#novoJogoBtn');
+novoTabuleiroBtn.addEventListener('click', fetchPokemon);
+
+const copyGame = document.querySelector('#copiarTabuleiro');
+copyGame.addEventListener('click', copiaCodigo);
+
+const loadGameBtn = document.getElementById('carrgarTabuleiroBtn');
+loadGameBtn.addEventListener('click', getGameCodeReady);
